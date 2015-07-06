1f093e1bc9d9758092150095d723e81811a6cd8d
unknown <preeti_j@DS-D8CB8A29CF59.HCLT.CORP.HCL.IN>
2015-07-06 11:28:06 +0530

    commit project

---
+++ CoffeeDecoratorPattern/Beverage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeDecoratorPattern
+{
+    abstract class Beverage
+    {
+
+        public abstract string GetDesc();
+        
+        public abstract int GetCost();
+    }
+
+    class Espresso : Beverage
+    {      
+
+        public override int GetCost()
+        {
+            return 9;
+        }
+
+        public override string GetDesc()
+        {
+            return "Espresso ";
+        }
+    }
+    class Latte : Beverage
+    {
+        public override string GetDesc()
+        {
+            return "Latte ";
+        }
+
+        public override int GetCost()
+        {
+            return 11;
+        }
+    }
+}
